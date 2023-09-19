de3c22355245abc0bb3e4c6c108741867ac77121
Matt Barry <matt@hazelmollusk.org>
2023-09-19 09:32:57 -0400

    .

--- .bash_include.sh
+++ .bash_include.sh
@@ -2,11 +2,10 @@ X=`realpath "${BASH_SOURCE[0]}"` X=`dirname "$X"` X=`realpath "$X/../.."` DOT_RO
 export DOT_ROOT
 export DOT_HOST=`hostname -s`
 export DOT_USER=`whoami`
-export TASKDDATA="/var/lib/tasjd"
+export TASKDDATA="/var/lib/taskd"
 
 function optsrc() {
   while [ -n "$1" ]; do
-  #echo -n "INCLUDING $1... "
     [ -f "$1" ] && source "$1" #&& echo '$' || echo '!'
     shift
   done
