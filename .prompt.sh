1e18583426aad86dd8879d5976a63cc0ee2a4219
Matt Barry <matt@hazelmollusk.org>
2023-10-08 09:02:01 -0400

    auto virtualenv activation

--- .prompt.sh
+++ .prompt.sh
@@ -22,36 +22,48 @@ COL_BULLET="${BOLD}${DIM}${CYAN}"
 optsrc "$HOME/.hosts.sh"
 
 function prompt_pre() {
+  which deactivate >/dev/null 2>&1 && deactivate
+  BASE=$(basename $PWD)
+  for j in env venv; do
+    for i in "$PWD/.$j" "$HOME/.$j/$BASE"; do
+      optsrc "$i/bin/activate"
+    done
+  done
+
   unset GIT
   declare -A GIT
-  GIT_STATUS=`git status -b --porcelain=1 2>&1`
+  GIT_STATUS=$(git status -b --porcelain=1 2>&1)
   if [[ ! $GIT_STATUS =~ fatal ]]; then
-      GIT[NAME]=`git config --get remote.origin.url | sed -e 's/^.*\/\([^/.]*\).*/\1/' 2>&1`
-      GIT[BRANCH]=`git branch --show-current 2>&1`
-      if [[ $GIT_STATUS =~ behind ]]; then GIT[BEHIND]="»";     fi
-      if [[ $GIT_STATUS =~ ahead  ]]; then GIT[AHEAD]="«";      fi
-      if [[ $GIT_STATUS =~ \?\?   ]]; then GIT[UNTRACKED]="?";  fi
-      if [[ $GIT_STATUS =~ \ M\   ]]; then GIT[MODIFIED]="¤";   fi
+    GIT[NAME]=$(git config --get remote.origin.url | sed -e 's/^.*\/\([^/.]*\).*/\1/' 2>&1)
+    GIT[BRANCH]=$(git branch --show-current 2>&1)
+    if [[ $GIT_STATUS =~ behind ]]; then GIT[BEHIND]="«"; fi
+    if [[ $GIT_STATUS =~ ahead ]]; then GIT[AHEAD]="»"; fi
+    if [[ $GIT_STATUS =~ \?\? ]]; then GIT[UNTRACKED]="?"; fi
+    if [[ $GIT_STATUS =~ \ M\  ]]; then GIT[MODIFIED]="¤"; fi
   fi
-  DDATE=`ddate +%A 2>/dev/null || date +%A 2>/dev/null`
-  TIME=`date +%I%M`
-  DISPLAY_PATH=$(p="${PWD#${HOME}}"; [ "${PWD}" != "${p}" ] && printf "~";IFS=/; for q in ${p:1}; do printf /${q:0:1}; done; printf "${q:1}")
+  DDATE=$(ddate +%A 2>/dev/null || date +%A 2>/dev/null)
+  TIME=$(date +%I%M)
+  DISPLAY_PATH=$(
+    p="${PWD#${HOME}}"
+    [ "${PWD}" != "${p}" ] && printf "~"
+    IFS=/
+    for q in ${p:1}; do printf /${q:0:1}; done
+    printf "${q:1}"
+  )
   PS1="\033]0;$USER@$HOST: ${DISPLAY_PATH} | ${GIT[ORIGIN]} ${GIT[BRANCH]:+(${GIT[BRANCH]})} ${GIT[UNTRACKED]:+\?}${GIT[MODIFIED]:+*}\007\
 ${COL_BUMP}▄\
 ${COL_DATE}\
-$SPACE2$DDATE${SPACE}${COL_BAR}│$SPACE${COL_TIME}$TIME$SPACE2\
-${COL_FADE1}▒░\
+$SPACE2$DDATE${SPACE}${COL_BAR}│$SPACE${COL_TIME}$TIME$SPACE2${COL_FADE1}▒░\
 ${debian_chroot:+($debian_chroot) }\
 ${VIRTUAL_ENV:+($(basename $VIRTUAL_ENV)) }\
 ${COL_USER}\
 $SPACE\u\
 ${COL_HOST}\
-\h$SPACE\
-${NORMAL}${CYAN}▒░\
+\h$SPACE${NORMAL}${CYAN}▒░\
 $NORMAL$SPACE${GIT[NAME]}${COL_BULLET} ■ $NORMAL$BOLD$WHITE${GIT[BRANCH]}\
+$BOLD$DIM$BLUE${GIT[BEHIND]}\
 $SPACE$BOLD$YELLOW${GIT[MODIFIED]}\
 $BOLD$RED${GIT[UNTRACKED]}\
-$BOLD$DIM$BLUE${GIT[BEHIND]}\
 $BOLD$DIM$GREEN${GIT[AHEAD]}\
 
 ${COL_BUMP}▀$NORMAL ${DISPLAY_PATH} \
@@ -59,7 +71,7 @@ $NORMAL$BOLD\$$NORMAL "
 
   cols=$(tput cols)
   if [[ -v simple || $cols -lt 60 ]]; then
-  PS1="\
+    PS1="\
 ${VIRTUAL_ENV:+$FG4($WHITE$(basename $VIRTUAL_ENV)$FG4) }\
 $GREEN\u\
 $PURPLE@\
